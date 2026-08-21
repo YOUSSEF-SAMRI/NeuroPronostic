7ad48e908b5fc246c79a560ee52d65faa24395be
youssef samri <youssef.samri77@gmail.com>
2026-08-22 00:13:37 +0100

    Move DB credentials to config.py

--- models/database.py
+++ models/database.py
@@ -1,11 +1,12 @@
 import psycopg2
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 
 def get_connections():
     return psycopg2.connect(
-        host="localhost",
-        port="5432",
-        dbname="neuropronostic_db",
-        user="postgres",
-        password="youssef"  
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
